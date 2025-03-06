530deee25aa03c2863730008d7e3ae77acb782d5
Ben Roberts <ben.roberts@rapdev.io>
2025-03-06 08:52:56 -0500

    fix various build issues

--- datadog-agent/run.sh
+++ datadog-agent/run.sh
@@ -2,7 +2,11 @@
 
 export DD_API_KEY=$(bashio::config 'api_key')
 export DD_SITE=$(bashio::config 'site')
+export DD_DOGSTATSD_NON_LOCAL_TRAFFIC= "true"
+
+# workaround for environment variable timing issue during agent startup
+echo "hostname: homeassistant" >> /etc/datadog-agent/datadog.yaml
 
 # Execute the original Datadog Agent entrypoint
-exec /entrypoint.sh
+exec /init
 
