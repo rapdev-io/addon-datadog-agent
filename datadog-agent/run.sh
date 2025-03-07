05cbc5fd46aad610f2b16fa628ed47525d990a74
Ben Roberts <ben.roberts@rapdev.io>
2025-03-07 15:04:52 -0500

    update run.sh

--- datadog-agent/run.sh
+++ datadog-agent/run.sh
@@ -3,10 +3,17 @@
 export DD_API_KEY=$(bashio::config 'api_key')
 export DD_SITE=$(bashio::config 'site')
 export DD_DOGSTATSD_NON_LOCAL_TRAFFIC= "true"
+export DD_CONTAINER_RUNTIME="docker"
 
-# workaround for environment variable timing issue during agent startup
-echo "hostname: homeassistant" >> /etc/datadog-agent/datadog.yaml
+# workaround for environment variable timing during agent init
+cat <<EOF > /etc/datadog-agent/datadog.yaml
+hostname: homeassistant
+
+dogstatsd_non_local_traffic: true
+
+apm_config:
+  enabled: false
+EOF
 
 # Execute the original Datadog Agent entrypoint
 exec /init
-
