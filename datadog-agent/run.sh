caf6af650bd69322f4efde5edba4c5fc01643c14
Ben Roberts <ben.roberts@rapdev.io>
2025-03-08 19:34:30 -0500

    make hostname configurable

--- datadog-agent/run.sh
+++ datadog-agent/run.sh
@@ -2,12 +2,13 @@
 
 export DD_API_KEY=$(bashio::config 'api_key')
 export DD_SITE=$(bashio::config 'site')
+DD_HOSTNAME=$(bashio::config 'hostname')
 export DD_DOGSTATSD_NON_LOCAL_TRAFFIC= "true"
 export DD_CONTAINER_RUNTIME="docker"
 
 # workaround for environment variable timing during agent init
 cat <<EOF > /etc/datadog-agent/datadog.yaml
-hostname: homeassistant
+hostname: $DD_HOSTNAME
 
 dogstatsd_non_local_traffic: true
 
